fdddbe22ac7390d6bdae59a27946d54deaf5bcff
FNahji <fathimanusra803@gmail.com>
2026-05-07 18:02:54 +0530

    Add end timestamp and save results to a file.

--- port_scanner.py
+++ port_scanner.py
@@ -4,6 +4,9 @@ import datetime
 target= input("Enter target IP adress: ")
 
 print(f"Scanning {target}")
+print(f"Scan started at {datetime.datetime.now()}")
+
+open_ports=[]
 
 
 for port in range(1,1025):
@@ -11,6 +14,14 @@ for port in range(1,1025):
     s.settimeout(0.1)
     if s.connect_ex((target,port))==0:
         print(f"Port {port} is open")
+        open_ports.append(port)
     s.close()
 
-print(f"Scan started at : {datetime.datetime.now()}")
+print(f"Scan ended at : {datetime.datetime.now()}")
+
+with open ("results.txt","w") as f:
+    f.write(f"Scan results for {target}\n")
+    for port in open_ports:
+        f.write(f"Port {port} is open\n")
+
+print("Results saved to results.txt")
